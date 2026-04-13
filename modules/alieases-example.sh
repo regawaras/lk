f69836792bd9d8c137eafa31cf0cf093a6bd971e
Regawaras <97470734+regawaras@users.noreply.github.com>
2026-04-13 11:24:31 +0700

    Create alieases-example.sh

---
+++ modules/alieases-example.sh
@@ -0,0 +1,9 @@
+# Fallback Backward-Compatibility Aliases
+#alias clk='lkc'
+#alias clkm='lkc -m'
+#alias clks='lkcs'
+#alias clkh='lkch'
+
+alias lkcm='lkc -m'
+alias lkcy='lkc -y'
+alias lkca='lkc -a'
